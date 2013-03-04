58514c44d7a3c9ad9894c0cf77168406d47e94bc
arisfatahillah <arisfatahillah@gmail.com>
2013-03-05 06:02:48 +0700

    upload kabeh boy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cobarailsgit::Application.config.secret_token = '4a5d2fa94a6419d3bce0e5b6e9b11c8fca255dfab9c9fdf9ea6a0d6c6cca863bfe3593d4122392cd13ba03515cb28969c7d5414ace452f7c0c7c60c6af54ce17'
